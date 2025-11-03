fa4c4587f9087c5b1541c457d84e038bf1aa1b2f
Kobina Otchere <kobinaotchere@Kobinas-MacBook-Pro.local>
2025-11-03 22:27:43 +0000

    basic set up completed.

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
-import { data } from "./content";
+const { data, reviews } = require("./content");
 
-export const renderContent = () => {
-    data.map(upgrade => `
+const renderContent = () => {
+    const cont = data.map(upgrade => `
         <div>
             <div>
                 <img src="${upgrade.image}">
@@ -9,4 +9,17 @@ export const renderContent = () => {
             <a href="${upgrade.url}">${upgrade.text}</a>
         </div>
         `).join('');
-}
+    return cont;
+};
+
+const renderReviews = () => {
+    const revs = reviews.map(review => `
+        <li>
+            "${review.text}"
+            <p>${review.rating}</p>
+        </li>
+        `).join('');
+    return revs;
+};
+
+module.exports = { renderContent, renderReviews };
